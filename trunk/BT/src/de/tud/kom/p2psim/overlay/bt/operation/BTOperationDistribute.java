c0cf7a84b28d961e8e9a41bbef26e1323551d3ff
chemakamacho <chemakamacho@9052b976-983b-0410-96ff-a197f3012145>
2008-02-28 13:00:01 +0000

    PeerfactSim 5.0 compatible BT updated and working, no related with previous revisions

---
+++ trunk/BT/src/de/tud/kom/p2psim/overlay/bt/operation/BTOperationDistribute.java
@@ -0,0 +1,120 @@
+package de.tud.kom.p2psim.overlay.bt.operation;
+
+
+
+import java.util.Collection;
+
+import org.apache.log4j.Logger;
+
+import de.tud.kom.p2psim.api.application.Application;
+import de.tud.kom.p2psim.api.common.Operation;
+import de.tud.kom.p2psim.api.common.OperationCallback;
+import de.tud.kom.p2psim.impl.util.logging.SimLogger;
+import de.tud.kom.p2psim.overlay.bt.BTContact;
+import de.tud.kom.p2psim.overlay.bt.BTDataStore;
+import de.tud.kom.p2psim.overlay.bt.BTDocument;
+import de.tud.kom.p2psim.overlay.bt.BTPeerDistributeNode;
+import de.tud.kom.p2psim.overlay.bt.BTPeerSearchNode;
+import de.tud.kom.p2psim.overlay.bt.BTTorrent;
+
+
+/**
+ * This method starts the upload, download and all related operations.
+ * @param <OwnerType> the class of the component that owns this operation
+ * @author Jan Stolzenburg
+ */
+public class BTOperationDistribute<OwnerType extends Application>  extends BTOperation<OwnerType, BTDocument> implements OperationCallback<BTDocument> {
+	
+	
+	
+	private BTDocument itsResult;
+	
+	private BTTorrent itsTorrent;
+	
+	private BTPeerSearchNode itsDhtNode;
+	
+	private BTPeerDistributeNode itsDistributionNode;
+	
+//	private BTOperationManager itsOperationManager;
+	
+	private BTOperationSendStatistic<BTPeerSearchNode> itsSendStatisticOperation;
+	
+	private int itsLookupOperationID;
+	
+	private BTDataStore itsDataBus;
+	
+	static final Logger log = SimLogger.getLogger(BTOperationDistribute.class);
+	
+	
+	
+	public BTOperationDistribute(BTDataStore theDataBus, BTTorrent theTorrent, BTPeerSearchNode theDhtNode, BTPeerDistributeNode theDistributionNode, OwnerType theOwningComponent, OperationCallback<BTDocument> theOperationCallback) {
+		super(theOwningComponent, theOperationCallback);
+		this.itsDataBus = theDataBus;
+		this.itsTorrent = theTorrent;
+		this.itsDhtNode = theDhtNode;
+		this.itsDistributionNode = theDistributionNode;
+	}
+	
+	@Override
+	protected void execute() {
+		this.step1();
+	}
+	
+	@Override
+	public BTDocument getResult() {
+		return this.itsResult;
+	}
+	
+	protected void setResult(BTDocument theResult) {
+		this.itsResult = theResult;
+	}
+	
+	@Override
+	protected void operationTimeoutOccured() {
+		operationFinished(false);
+	}
+	
+	/**
+	 * When the operation is executed, it calls this method that starts a lookup operation.
+	 * This will return a list of peers that are currently part of this torrent.
+	 */
+	private void step1() {
+		this.itsLookupOperationID = this.itsDhtNode.valueLookup(this.itsTorrent, this); //Starts the search in the dht. Returns the search ID. The dht will call back, when it has found the information.
+//		this.itsOperationManager.registerOperationEventHandler(lookupOperationID, this); //We store the search ID. We need it, when the search has finished and calls back, telling us the result.
+	}
+	
+	/**
+	 * When the lookup operation is finished, this method gets called.
+	 * It starts the download, upload and the regular send of statistic data.
+	 * @param theDocumentHolder the list of peers that should have parts of this document
+	 */
+	@SuppressWarnings("unchecked")
+	private void step2(Collection<BTContact> theDocumentHolder) {
+		log.debug("Tracker successfully contacted. I received " + theDocumentHolder.size() + " new peers.");
+		
+		this.itsSendStatisticOperation = new BTOperationSendStatistic(this.itsDataBus, this.itsDistributionNode.getPort(), this.itsTorrent, this.itsDhtNode.getOverlayID(), this.itsDhtNode, this);
+		this.itsSendStatisticOperation.scheduleWithDelay(BTOperationSendStatistic.getPeriod());
+		
+		this.itsDistributionNode.downloadDocument(this.itsTorrent.getKey(), null, this); //I don't pass addresses of other peers, as I have the peer managers for this job!
+//		int downloadOperationID = this.itsDistributionNode.downloadDocument(this.itsTorrent.getKey(), null, this); //I don't pass addresses of other peers, as I have the peer managers for this job!
+//		this.itsOperationManager.registerOperationEventHandler(downloadOperationID, this);
+		
+		this.itsDistributionNode.uploadDocument(this.itsTorrent.getKey(), this);
+//		int uploadOperationID = this.itsDistributionNode.uploadDocument(this.itsTorrent.getKey(), this);
+//		this.itsOperationManager.registerOperationEventHandler(uploadOperationID, this);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void calledOperationSucceeded(Operation<BTDocument> theOperation) {
+		if (this.itsLookupOperationID == theOperation.getOperationID())
+			this.step2((Collection<BTContact>)theOperation.getResult());
+	}
+	
+	public void calledOperationFailed(Operation<BTDocument> theOperation) {
+		if (this.itsLookupOperationID == theOperation.getOperationID()) {
+			log.error("Failed connecting to tracker. Aborting.");
+			this.operationFinished(false);
+		}
+	}
+	
+}
---
+++ trunk/BT/src/uc3m/netcom/peerfactsim/overlay/bt/algorithm/BTAlgorithmUploadPeerNumSelection.java
@@ -0,0 +1,254 @@
+/*
+ * BTAlgorithmUploadPeerNumSelection.java
+ *
+ * Created on 17 de enero de 2008, 14:17
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package uc3m.netcom.peerfactsim.overlay.bt.algorithm;
+
+/**
+ * This class contains our novel number of unchoking peers
+ * algorithm.
+ *
+ * @author JMCamacho
+ */
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.math.random.RandomGenerator;
+import org.apache.log4j.Logger;
+
+import de.tud.kom.p2psim.impl.simengine.Simulator;
+import de.tud.kom.p2psim.impl.util.logging.SimLogger;
+import de.tud.kom.p2psim.overlay.bt.BTConstants;
+import de.tud.kom.p2psim.overlay.bt.BTContact;
+import de.tud.kom.p2psim.overlay.bt.BTDocument;
+import de.tud.kom.p2psim.overlay.bt.BTInternStatistic;
+import de.tud.kom.p2psim.overlay.bt.manager.BTConnectionManager;
+import de.tud.kom.p2psim.overlay.bt.algorithm.BTAlgorithmChoking;
+
+public class BTAlgorithmUploadPeerNumSelection {
+    
+	private BTInternStatistic itsStatistic;
+	private RandomGenerator itsRandomGenerator;
+	private boolean itsIsSetup = false;
+                
+        /*****************
+         *Hay que modificar estas constantes en BTConstants para ajustarlas al
+         *nuevo diseno.
+         */
+        
+        private static final long itsPeriod = BTConstants.CHOKING_OPTIMISTIC_CHOKING_RECALC_PERIOD;
+        private static final int INC_STATE = 0;
+        private static final int EQ_STATE = 1;
+        private static final int DEC_STATE = 2;
+        
+        private double itsMaxUR;
+        private double itsMaxDR;
+        private long lastExecutionTime = -1;
+        private double lastDownloadRate = -1.0;
+        private double lastUploadRate = -1.0;
+        private int currentState = EQ_STATE;
+        private int currentRU = BTConstants.CHOKING_NUMBER_OF_REGULAR_UNCHOKES;
+        private int currentOU = BTConstants.CHOKING_NUMBER_OF_OPTIMISTIC_UNCHOKES;
+        private int blockSize = (int) java.lang.Math.pow(2,BTConstants.DOCUMENT_DEFAULT_BLOCK_EXPONENT);
+        static final Logger log = SimLogger.getLogger(BTAlgorithmChoking.class);
+               
+        private static final byte[][] stateMachine = new byte[][]{
+                {1,1,1,0,1,2,2,2,0,2,2,0,1,1,1,2,2,0},
+                {1,1,1,0,1,2,2,2,0,2,2,0,1,1,1,2,2,0},
+                {1,1,1,1,0,2,2,2,0,2,2,0,1,1,1,0,1,2},
+                {1,1,1,1,0,2,2,2,0,2,2,0,1,1,1,1,1,1},
+                {1,1,1,1,0,2,2,0,0,2,0,0,1,1,1,1,1,1},
+                {1,1,1,1,0,2,2,0,0,2,0,0,1,1,1,1,1,1}
+        };
+     
+    
+	/**
+	 * Use this method to use the algorithm.
+         * It will set up the amount of unchoked peers that will be unchoked when the
+         * choking operation executes the next time.
+	 */
+    
+   /*   Creo que incrementar el numero de conexiones siendo seed no tiene mucho sentido. A menos que se quiera
+      probar el rendimiento global si los seeder sirve a m�s peers, aunque esto puede depender de otros
+      factores tales como el ancho de banda que se desee proporcionar a cada peer.*/
+
+    
+	public int[] establishUnchokedNum(Collection<BTContact> theContacts) {
+		if (! this.isSetup()) {
+			log.error("You have to setup this algorithm first!");
+			throw new RuntimeException("You have to setup this algorithm first!");
+		}
+		if (theContacts.isEmpty())
+			//return new int[]{currentRU,currentOU};
+                        return new int[]{5,1};
+                /*
+                double currentDR = downloadRate(theContacts);
+                double currentUR = uploadRate(theContacts);
+                int newState = stateMachine[calcRow(currentDR)][calcColumn(currentUR)];            
+                updateAlgorithm(currentDR, currentUR, newState);
+                //return new int[]{currentRU,currentOU};
+                return new int[]{15,1};
+	}       */
+                return new int[]{5,1};
+	}
+
+       
+	public void setup(BTInternStatistic theStatistic, RandomGenerator theRandomGenerator,double maxUR, double maxDR) {
+		this.itsRandomGenerator = theRandomGenerator;
+		this.itsStatistic = theStatistic;
+		this.itsIsSetup = true;
+                this.itsMaxUR = maxUR;
+                this.itsMaxDR = maxDR;
+	}
+
+        
+        private void updateAlgorithm(double currentDR, double currentUR,int newState){
+                
+               /* System.out.println("El nuevo Estado es = "+ newState);
+                System.out.println("DRold = "+this.lastDownloadRate);
+                System.out.println("DRnew = "+currentDR);
+                System.out.println("URold = "+this.lastUploadRate);
+                System.out.println("URnew = "+currentUR);*/
+            
+                this.lastDownloadRate = currentDR;
+                this.lastUploadRate = currentUR;                
+                this.lastExecutionTime = Simulator.getCurrentTime();
+                this.currentState = newState;
+          
+                switch(newState){
+                    case BTAlgorithmUploadPeerNumSelection.INC_STATE:
+                        this.currentRU++;
+                        this.currentOU++;
+                        break;
+                    case BTAlgorithmUploadPeerNumSelection.DEC_STATE:
+                        if(currentRU > 3){
+                            this.currentRU--;
+                            this.currentOU--;
+                        }
+                        break;
+                    case BTAlgorithmUploadPeerNumSelection.EQ_STATE:
+                        
+                }
+        }
+        
+        /** This method returns total download rate obtained during the last 30 seg in
+         *bytes per second.
+         *
+         *@return double Download Rate in packets per second.
+         */
+        
+        private double downloadRate(Collection<BTContact> contacts){
+            
+            int packets = 0;
+            
+            for(BTContact oneContact : contacts){
+            
+                //LinkedList<Long> times = (LinkedList) itsStatistic.getDownloadStatisticForPeer(oneContact);                
+                List<Long> times1 = itsStatistic.getDownloadStatisticForPeer (oneContact);
+                LinkedList<Long> times = new LinkedList<Long>(times1);
+                while(times.size() > 0 && times.getLast() >= lastExecutionTime){
+                  packets++;
+                  times.removeLast();                                      
+                }
+            }            
+            //System.out.println("Packets = "+ packets);
+            //System.out.println("Block Size = "+ blockSize);
+            //System.out.println("Current Time = "+ Simulator.getCurrentTime());
+            //System.out.println("LastExecutionTime = "+ this.lastExecutionTime);
+            double a = packets*blockSize;
+            double b = Simulator.getCurrentTime()-this.lastExecutionTime;
+            double c = a/b;
+            //System.out.println("a = "+a);
+            //System.out.println("b = "+b);
+            //System.out.println("c = "+c);
+            return c;
+            //return (packets*blockSize)/(Simulator.getCurrentTime()-this.lastExecutionTime);
+        }
+
+        
+        private double uploadRate(Collection<BTContact> contacts){
+            
+            int packets = 0;
+            
+            for(BTContact oneContact : contacts){         
+                packets += uploadedSince(lastExecutionTime,oneContact);
+            }
+            double a = packets*blockSize;
+            double b = Simulator.getCurrentTime()-lastExecutionTime;
+            double c = a/b;
+            return c;
+            //return (packets*blockSize)/(Simulator.getCurrentTime()-lastExecutionTime);
+        }
+        
+         
+        
+ 
+        
+        private int calcRow(double currentUR){
+            
+            if(currentUR > this.lastUploadRate){
+                
+                if(currentUR == this.itsMaxUR) return 0;
+                else return 1;
+                    
+            }else if(currentUR < this.lastUploadRate){
+                
+                if(this.lastUploadRate == this.itsMaxUR) return 2;
+                else return 3;
+                
+            }else{
+                
+                if(currentUR == this.itsMaxUR) return 4;
+                else return 5;
+            }
+        }
+        
+        
+        private int calcColumn(double currentDR){
+            
+            if(currentDR > this.lastDownloadRate){
+                
+                if(currentDR == this.itsMaxDR) return 0+this.currentState;
+                else return 3+this.currentState;
+                    
+            }else if(currentDR < this.lastDownloadRate){
+                
+                if(this.lastDownloadRate == this.itsMaxDR) return 6+currentState;
+                else return 9+currentState;
+                    
+            }else{                
+                
+                if(currentDR == this.itsMaxDR) return 12+currentState;
+                else return 15+currentState;
+            }
+        }
+        
+              
+	public boolean isSetup() {
+		return this.itsIsSetup;
+	}
+	
+
+	private int uploadedSince(long theTime, BTContact theContact) {
+		int result = 0;
+		List<Long> uploadPiecesTimes = this.itsStatistic.getUploadStatisticForPeer(theContact);
+		int currentPosition = uploadPiecesTimes.size() - 1;
+		while ((currentPosition >= 0) && (uploadPiecesTimes.get(currentPosition) >= theTime)) {
+			result += 1;
+			currentPosition -= 1;
+		}
+		return result;
+	}
+	
+	
+}
