843927719d989a5e8a2c684bd6be3c23bb5628af
chemakamacho <chemakamacho@9052b976-983b-0410-96ff-a197f3012145>
2008-07-07 15:52:17 +0000

    Raw code for prospective real BT client implementation

---
+++ trunk/BT3/src/de/tud/kom/p2psim/overlay/bt/manager/BTConnectionManager.java
@@ -0,0 +1,100 @@
+package de.tud.kom.p2psim.overlay.bt.manager;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+import de.tud.kom.p2psim.overlay.bt.BTConnection;
+import de.tud.kom.p2psim.overlay.bt.BTContact;
+
+public class BTConnectionManager {
+	
+	private Map<BTContact, BTConnection> itsConnections;
+	
+	private BTContact itsOwnContact;
+	
+	public BTConnectionManager(BTContact theLocalAddress) {
+		this.itsOwnContact = theLocalAddress;
+		this.itsConnections = new HashMap<BTContact, BTConnection>(50);
+	}
+	
+	public BTContact getLocalAddress() {
+		return this.itsOwnContact;
+	}
+	
+	public void addConnection(BTContact theOtherSide) {
+		if (this.itsConnections.containsKey(theOtherSide))
+			return;
+		BTConnection newConnection = new BTConnection(this.itsOwnContact, theOtherSide);
+		this.itsConnections.put(theOtherSide, newConnection);
+	}
+	
+	public BTConnection getConnection(BTContact theOtherSide) {
+		if (! this.itsConnections.containsKey(theOtherSide))
+			return null;
+		return this.itsConnections.get(theOtherSide);
+	}
+	
+	public boolean isConnectionRegisteredTo(BTContact theOtherSide) {
+		return this.itsConnections.containsKey(theOtherSide);
+	}
+	
+//	public void handshakingWith(BTContact theOtherSide) {
+//		this.itsConnections.get(theOtherSide).handshaking();
+//	}
+//	
+//	public void connectedTo(BTContact theOtherSide) {
+//		this.itsConnections.get(theOtherSide).connected();
+//	}
+//	
+//	public void disconnectedFrom(BTContact theOtherSide) {
+//		this.itsConnections.get(theOtherSide).disconnected();
+//	}
+//	
+//	public boolean isConnectedTo(BTContact theOtherSide) {
+//		if (! this.itsConnections.containsKey(theOtherSide))
+//			return false;
+//		return this.itsConnections.get(theOtherSide).isConnected();
+//	}
+//	
+//	public boolean isHandshakingWith(BTContact theOtherSide) {
+//		if (! this.itsConnections.containsKey(theOtherSide))
+//			return false;
+//		return this.itsConnections.get(theOtherSide).isHandshaking();
+//	}
+//	
+//	public long isHandshakingSince(BTContact theOtherSide) {
+//		if (! this.itsConnections.containsKey(theOtherSide))
+//			return BTConnection.getHandshakingTimeFalseValue();
+//		return this.itsConnections.get(theOtherSide).isHandshakingSince();
+//	}
+	
+	public Collection<BTContact> getConnectedContacts() {
+		Collection<BTContact> result = new LinkedList<BTContact>();
+		for (BTContact aContact : this.itsConnections.keySet()) {
+			if (this.itsConnections.get(aContact).isConnected())
+				result.add(aContact);
+		}
+		return result;
+	}
+	
+	public int getNumberOfConnectedContacts() {
+		int result = 0;
+		for (BTContact aContact : this.itsConnections.keySet()) {
+			if (this.itsConnections.get(aContact).isConnected())
+				result += 1;
+		}
+		return result;
+	}
+	
+	public Collection<BTContact> getHandshakingContacts() {
+		Collection<BTContact> result = new LinkedList<BTContact>();
+		for (BTContact aContact : this.itsConnections.keySet()) {
+			if (this.itsConnections.get(aContact).isHandshaking())
+				result.add(aContact);
+		}
+		return result;
+	}
+	
+}
---
+++ trunk/BT3/src/de/tud/kom/p2psim/overlay/bt/message/BTMessage.java
@@ -0,0 +1,103 @@
+package de.tud.kom.p2psim.overlay.bt.message;
+
+import de.tud.kom.p2psim.api.common.Message;
+import de.tud.kom.p2psim.api.overlay.OverlayID;
+import de.tud.kom.p2psim.api.transport.TransProtocol;
+import de.tud.kom.p2psim.impl.overlay.AbstractOverlayMessage;
+import de.tud.kom.p2psim.impl.simengine.Simulator;
+
+/**
+ * This is the abstract super class of all BitTorrent Messages.
+ * @author Jan Stolzenburg
+ */
+public abstract class BTMessage extends AbstractOverlayMessage<OverlayID> {
+	
+	/**
+	 * The BitTorrent type of the message.
+	 * @author Jan Stolzenburg
+	 */
+	public enum Type {CHOKE, UNCHOKE, INTERESTED, UNINTERESTED, HAVE, BITFIELD, REQUEST, PIECE, CANCEL, KEEPALIVE, HANDSHAKE, TRACKER_REQUEST, TRACKER_REPLY}
+	
+	/**
+	 * The BitTorrent type of the message.
+	 */
+	private Type itsType;
+	
+	private TransProtocol itsTransportProtocol;
+	
+	private int itsSize;
+	
+	/**
+	 * Stores, if the data got corrupted or not.
+	 */
+	private boolean itsDataCorrect;
+	
+	/**
+	 * When has the message bin created and send?
+	 */
+	private long itsTimestamp;
+	
+//	private boolean received;
+	
+	public BTMessage(Type theType, TransProtocol theTransportProtocol, boolean theDataCorrect, int theSize, OverlayID theSender, OverlayID theReceiver) {
+		super(theSender, theReceiver);
+		this.itsSize = theSize;
+		this.itsType = theType;
+		this.itsTransportProtocol = theTransportProtocol;
+		this.itsDataCorrect = theDataCorrect;
+		this.itsTimestamp = Simulator.getCurrentTime();
+//		this.received = false;
+	}
+	
+	public boolean isDataCorrect() {
+		return this.itsDataCorrect;
+	}
+	
+	/**
+	 * Call this method, if you send this message with delay after its creation.
+	 */
+	public void updateTimestamp() {
+		this.itsTimestamp = Simulator.getCurrentTime();
+//		this.received = false;
+	}
+	
+	public long getTimestamp() {
+		return this.itsTimestamp;
+	}
+	
+//	/**
+//	 * Call this method, when you receive this message.
+//	 */
+//	public void received() {
+//		this.received = true;
+//	}
+//	
+//	public boolean isReceived() {
+//		return this.received;
+//	}
+	
+	/**
+	 * @return the BitTorrent message type.
+	 */
+	public Type getType() {
+		return this.itsType;
+	}
+	
+	public TransProtocol getTransportProtocol() {
+		return this.itsTransportProtocol;
+	}
+	
+	@Override
+	public String toString() {
+		return "[BTMessage| Type: " + this.getType() + "; Size: " + this.getSize() + "; From: " + this.getSender() + "; To: " + this.getReceiver() + "]";
+	}
+	
+	public Message getPayload() {
+		return null;
+	}
+
+	public long getSize() {
+		return this.itsSize;
+	}
+	
+}
