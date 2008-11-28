9afb2e02706a25be737e9b3c90f6a6c2706804f8
chemakamacho <chemakamacho@9052b976-983b-0410-96ff-a197f3012145>
2008-11-28 14:05:45 +0000

    V0.2 Efficiency and robustness improved. Test shown that up to 200 torrent info file can be downloaded in about 20min. without program failure.

--- trunk/Crawler/src/crawler/Connection.java
+++ trunk/Crawler/src/crawler/Connection.java
@@ -2,18 +2,14 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package crawler;
 
 import jBittorrentAPI.*;
-import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Iterator;
-//import java.util.*;
-//import javax.swing.event.EventListenerList;
 import java.net.UnknownHostException;
 import java.io.InputStream;
 import java.io.IOException;
@@ -23,123 +19,109 @@ import java.io.PrintWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.net.Inet4Address;
+
 /**
  *
  * @author jmcamacho
  */
-public class Connection extends Thread{// extends PeerUpdater{
+public class Connection extends Thread {// extends PeerUpdater{
 
     private File dat_file;
-    private HashSet<String> table;
     private byte[] idf;
     private TorrentFile torrentf;
-    private long downloaded = 0;
-    private long uploaded = 0;
-    private long left = 0;
     private String event = "&event=started";
-    private int listeningPort = 6881;
     private int interval = 150;
     private boolean first = true;
     private boolean end = false;
-    private LinkedHashMap<String,Peer> peerL;
+    private LinkedHashMap<String, Peer> peerL;
     private long num_want;
     private String local_addr;
-    private int attemps = 0;
-    
-    public Connection(byte[] id,TorrentFile t,String save_path,String local_addr){
-        
-       // super(id,t);
+
+    public Connection(byte[] id, TorrentFile t, String save_path, String local_addr) {
+
+        // super(id,t);
         this.torrentf = t;
         this.idf = id;
-        this.left = torrentf.total_length;
-        this.table = new HashSet<String>();
         dat_file = new File(save_path);
-        try{
+        try {
             dat_file.createNewFile();
-            }catch(IOException e){
-                System.out.println(e.getMessage());
-                e.printStackTrace();
-            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
         this.num_want = 0;
         this.local_addr = local_addr;
-        this.peerL = new LinkedHashMap<String,Peer>();
-        
+        this.peerL = new LinkedHashMap<String, Peer>();
+
     }
+
     /**
      * Thread method that regularly contact the tracker and process its response
      */
     @Override
-    
-    public void run(){
-        
+    public void run() {
+
         int tryNB = 0;
-        LinkedHashMap<String,Peer> auxL = new LinkedHashMap<String,Peer>();
+        LinkedHashMap<String, Peer> auxL = new LinkedHashMap<String, Peer>();
         byte[] b = new byte[0];
-        
+
         while (!this.end) {
             tryNB++;
 
             auxL = this.processResponse(this.contactTracker(idf,
-                    torrentf, this.downloaded,
-                    this.uploaded,
-                    this.left, this.event));
+                    torrentf, 0,
+                    0,
+                    this.torrentf.total_length, this.event));
             if (auxL != null) {
-                if (first) {
-                    this.event = "";
-                    first = false;
+                /*      if (first) {
+                this.event = "";
+                first = false;
                 }
                 
-                if(this.num_want==1){
-                    dat_file.delete();
-                    this.end = true;
-                    continue;
-                }
+                }*/
                 tryNB = 0;
                 Iterator it = auxL.keySet().iterator();
-                
-                while(it.hasNext()){
+
+                while (it.hasNext()) {
                     String key = (String) it.next();
-                    
-                    if(!peerL.containsKey(key)){
-                        peerL.put(key,auxL.get(key));
+
+                    if (!peerL.containsKey(key)) {
+                        peerL.put(key, auxL.get(key));
                     }
                 }
-                
-                
-                if(peerL.size() >= this.num_want){
-                    
-                    attemps = 0;
-                    try{
-                        this.end=true;
+
+
+                if (peerL.size() >= this.num_want) {
+
+                    try {
+                        this.end = true;
                         Iterator itf = peerL.values().iterator();
                         PrintWriter pw = new PrintWriter(dat_file);
-                        
-                        while(itf.hasNext()){
+
+                        while (itf.hasNext()) {
                             Peer p = (Peer) itf.next();
-                            if(!p.getIP().equals(local_addr)){
-                                pw.println(p.getIP()+" "+p.getPort());
+                            if (!p.getIP().equals(local_addr)) {
+                                pw.println(p.getIP() + " " + p.getPort());
                             }
                         }
-                        
-                        
+
+
                         pw.flush();
                         pw.close();
-                        synchronized(Crawler.class){
+                        synchronized (Crawler.class) {
                             Crawler.count++;
-                            System.out.println("New IP set fetched: "+Crawler.count);
+                            System.out.println("New IP set fetched: " + Crawler.count);
                         }
-                        
-                    }catch(Exception e){
+
+                    } catch (Exception e) {
                         System.out.println(e.getMessage());
                         e.printStackTrace();
                     }
-                    
-                }else{
-                    attemps++;
-                    if(attemps==5) this.end = true;
-                    continue;
+
+                } else {
+                    tryNB++;
                 }
+                
                 
                 try {
                     synchronized (b) {
@@ -148,30 +130,32 @@ public class Connection extends Thread{// extends PeerUpdater{
                 } catch (InterruptedException ie) {
                     System.out.println(ie.getMessage());
                     ie.printStackTrace();
+                    dat_file.delete();
+                    this.end = true;
+                    System.gc();
                 }
+
             } else {
                 dat_file.delete();
-                this.end=true;
+                this.end = true;
                 System.gc();
-//                try {
-//                    synchronized (b) {
-//                        b.wait(2000);
-//                    }
-//                    dat_file.createNewFile();
-//                } catch (InterruptedException ie) {
-//                    System.out.println(ie.getMessage());
-//                    ie.printStackTrace();
-//                } catch (IOException e){
-//                    System.out.println(e.getMessage());
-//                    e.printStackTrace();
-//                }
+                
+
             }
+
+            if (tryNB == 5) {
+                dat_file.delete();
+                this.end = true;
+                System.gc();
+                continue;
+            }
+
         }
-        
-            synchronized(Crawler.class){
-                Crawler.open_con--;
-                Crawler.class.notifyAll();
-            }
+
+        synchronized (Crawler.lock) {
+            Crawler.open_con--;
+            Crawler.lock.notify();
+        }
     }
 
     /**
@@ -181,45 +165,44 @@ public class Connection extends Thread{// extends PeerUpdater{
      * @param m The tracker response as a Map
      * @return LinkedHashMap A HashMap containing the peers and their ID as keys
      */
- 
-    
     public synchronized LinkedHashMap<String, Peer> processResponse(Map m) {
         LinkedHashMap<String, Peer> l = null;
         if (m != null) {
             if (m.containsKey("failure reason")) {
                 System.out.println("The tracker returns the following error message:" +
-                                      "\t'" +
-                                      new String((byte[]) m.get(
-                                              "failure reason")) +
-                                      "'");
+                        "\t'" +
+                        new String((byte[]) m.get(
+                        "failure reason")) +
+                        "'");
                 this.end = true;
                 return null;
             } else {
-                if (((Long) m.get("interval")).intValue() < this.interval)
+                if (((Long) m.get("interval")).intValue() < this.interval) {
                     this.interval = ((Long) m.get("interval")).intValue();
-                else
-                    this.interval *= 2;
-
-                this.num_want = ((Long)m.get("complete"))+((Long)m.get("incomplete"));
-                System.out.println("Number of Seeds: "+((Long)m.get("complete"))+" Number of Leechers: "+((Long)m.get("incomplete")));
+                }
                 
+                try {
+                    this.num_want = ((Long) m.get("complete")) + ((Long) m.get("incomplete"));
+                    if (this.num_want == 1) {
+                        return null;
+                    }
+                } catch (NullPointerException e) {
+                    return null;
+                }
+
+                System.out.println("Number of Seeds: " + ((Long) m.get("complete")) + " Number of Leechers: " + ((Long) m.get("incomplete")));
+
                 Object peers = m.get("peers");
                 ArrayList peerList = new ArrayList();
                 l = new LinkedHashMap<String, Peer>();
                 if (peers instanceof List) {
-                    peerList.addAll((List)peers);
+                    peerList.addAll((List) peers);
                     if (peerList != null && peerList.size() > 0) {
                         for (int i = 0; i < peerList.size(); i++) {
-                            String peerID = new String((byte[]) ((Map) (
-                                    peerList.
-                                    get(i))).
-                                    get(
-                                            "peer_id"));
-                            String ipAddress = new String((byte[]) ((Map) (
-                                    peerList.
-                                    get(
-                                            i))).
-                                    get("ip"));
+                            String peerID = new String((byte[]) ((Map) (peerList.get(i))).get(
+                                    "peer_id"));
+                            String ipAddress = new String((byte[]) ((Map) (peerList.get(
+                                    i))).get("ip"));
                             int port = ((Long) ((Map) (peerList.get(i))).get(
                                     "port")).intValue();
                             Peer p = new Peer(peerID, ipAddress, port);
@@ -231,9 +214,9 @@ public class Connection extends Thread{// extends PeerUpdater{
                     for (int i = 0; i < p.length; i += 6) {
                         Peer peer = new Peer();
                         peer.setIP(Utils.byteToUnsignedInt(p[i]) + "." +
-                                   Utils.byteToUnsignedInt(p[i + 1]) + "." +
-                                   Utils.byteToUnsignedInt(p[i + 2]) + "." +
-                                   Utils.byteToUnsignedInt(p[i + 3]));
+                                Utils.byteToUnsignedInt(p[i + 1]) + "." +
+                                Utils.byteToUnsignedInt(p[i + 2]) + "." +
+                                Utils.byteToUnsignedInt(p[i + 3]));
                         peer.setPort(Utils.byteArrayToInt(Utils.subArray(p,
                                 i + 4, 2)));
                         l.put(peer.toString(), peer);
@@ -241,8 +224,9 @@ public class Connection extends Thread{// extends PeerUpdater{
                 }
             }
             return l;
-        } else
+        } else {
             return null;
+        }
     }
 
     /**
@@ -256,19 +240,18 @@ public class Connection extends Thread{// extends PeerUpdater{
      * @param event String
      * @return A Map containing the decoded tracker response
      */
-    
     public synchronized Map contactTracker(byte[] id,
-                                           TorrentFile t, long dl, long ul,
-                                           long left, String event) {
+            TorrentFile t, long dl, long ul,
+            long left, String event) {
         try {
-            
+
             URL source = new URL(t.announceURL + "?info_hash=" +
-                                 t.info_hash_as_url + "&peer_id=" +
-                                 Utils.byteArrayToURLString(id) + "&port="+
-                                this.listeningPort +
-                                 "&downloaded=" + dl + "&uploaded=" + ul +
-                                 "&left=" +
-                                 left + "&numwant=200&compact=1" + event);
+                    t.info_hash_as_url + "&peer_id=" +
+                    Utils.byteArrayToURLString(id) + "&port=" +
+                    6881 +
+                    "&downloaded=" + dl + "&uploaded=" + ul +
+                    "&left=" +
+                    left + "&numwant=200&compact=1" + event);
             //System.out.println("Contact Tracker. URL source = " + source);   //DAVID
             URLConnection uc = source.openConnection();
             InputStream is = uc.getInputStream();
@@ -281,16 +264,13 @@ public class Connection extends Thread{// extends PeerUpdater{
             return m;
         } catch (MalformedURLException murle) {
             System.out.println("Tracker URL is not valid... Check if your data is correct and try again");
-            
         } catch (UnknownHostException uhe) {
             System.out.println("Tracker not available... Retrying...");
         } catch (IOException ioe) {
-            System.out.println("Tracker unreachable... Retrying"+ioe.getMessage());
-            ioe.printStackTrace();
+            System.out.println("Tracker unreachable... Retrying " + ioe.getMessage());
         } catch (Exception e) {
             System.out.println("Internal error");
         }
         return null;
     }
-    
 }
